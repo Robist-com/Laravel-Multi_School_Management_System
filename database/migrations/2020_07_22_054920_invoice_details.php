bd07dbc1909e81d87c708aa5fc9976a7c222343d
singhateh <3939919@gmail.com>
2020-07-30 20:06:59 +0700

    My sms project

--- database/migrations/2020_07_22_054920_invoice_details.php
+++ database/migrations/2020_07_22_054920_invoice_details.php
@@ -22,7 +22,7 @@ class InvoiceDetails extends Migration
             // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->unsignedBigInteger('student_fee_id')->nullable(); // student_fee_id
-            $table->foreign('student_fee_id')->references('student_fee_id')->on('student_fees')->onDelete('cascade');
+            // $table->foreign('student_fee_id')->references('student_fee_id')->on('student_fees')->onDelete('cascade');
 
             // $table->foreign('student_fee_id')->references('student_fee_id')->on('student_fees');
             // ->onDelete('cascade');
@@ -33,7 +33,7 @@ class InvoiceDetails extends Migration
             // ->onDelete('cascade');
 
             $table->unsignedBigInteger('invoice_id')->nullable(); // invoice_id
-            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+            // $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
             // $table->foreign('invoice_id')->references('id')->on('invoices');
             // ->onDelete('cascade');
 
