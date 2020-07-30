0dc6a85d10378538db58f39490a0ac56c05e3d37
singhateh <3939919@gmail.com>
2020-07-30 20:12:01 +0700

    My sms project

--- database/migrations/2020_07_30_110541_create_student_fees.php
+++ database/migrations/2020_07_30_110541_create_student_fees.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class StudentFees extends Migration
+class CreateStudentFees extends Migration
 {
     /**
      * Run the migrations.
