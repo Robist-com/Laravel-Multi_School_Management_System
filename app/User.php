073d0ffd547d2e4e8be4c6672bdb5bc6786f03a6
singhateh <3939919@gmail.com>
2020-07-30 21:19:24 +0700

    My sms project

--- app/User.php
+++ app/User.php
@@ -16,7 +16,13 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'role_id',
+        'teacher_id',
+        'email_verified_at',
+        'password',
+        'remember_token'
     ];
 
     /**
