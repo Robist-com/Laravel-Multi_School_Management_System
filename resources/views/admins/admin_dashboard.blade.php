bb592db980e06dddd314119bf117026f4e971ddb
singhateh <3939919@gmail.com>
2020-07-30 21:46:03 +0700

    My sms project

--- resources/views/admins/admin_dashboard.blade.php
+++ resources/views/admins/admin_dashboard.blade.php
@@ -298,7 +298,7 @@
       <!-- /.col -->
       <div class="col-md-3 col-sm-6 col-xs-12">
         <div class="info-box">
-          <span class="info-box-icon bg-blue"><i class="glyphicon glyphicon-tasks">{{$classasignCount}}</i></span>
+          <span class="info-box-icon bg-blue"><i class="glyphicon glyphicon-tasks"></i></span>
 
           <div class="info-box-content">
             <span class="info-box-text alert "> CLASS ASSIGNED'S</span>
